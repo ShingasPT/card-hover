eb7b799a4d9273acfa1de06d46d851c86357b5f9
ShingasPT <afonsohenriques661@gmail.com>
2023-10-03 22:54:56 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className='card-container'>
+        <div className="card card-1 " />
+        <div className="card card-2" />
+      </section>
+    </>
+  )
+}
+
+export default App
