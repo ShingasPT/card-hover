2258db4c4dd6c8f5c143b808ece3c440d00ed566
ShingasPT <afonsohenriques661@gmail.com>
2023-10-06 22:22:09 +0100

    v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,22 @@ function App() {
   return (
     <>
       <section className='card-container'>
-        <div className="card card-1 " />
-        <div className="card card-2" />
+        <div className="card">
+          <div className="card-info" >
+            <p className='title'>Salad</p>
+            <div className='text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab nisi corporis officiis temporibus natus deleniti suscipit ad ipsa cum optio hic unde dignissimos incidunt ullam magni, doloribus deserunt inventore quas!</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-info" >
+            <p className='title'>Chocolate</p>
+            <div className='text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab nisi corporis officiis temporibus natus deleniti suscipit ad ipsa cum optio hic unde dignissimos incidunt ullam magni, doloribus deserunt inventore quas!</div>
+          </div>
+        </div>
       </section>
     </>
   )
+
 }
 
 export default App
